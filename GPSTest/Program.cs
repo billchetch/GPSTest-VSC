2a4cbc5f98c2a7a71170bffdd684086b6d07027c
billchetch <45006134+billchetch@users.noreply.github.com>
2025-01-19 08:05:46 +0700

    sss

--- GPSTest/Program.cs
+++ GPSTest/Program.cs
@@ -13,35 +13,7 @@ namespace GPSTest;
 
 class Program
 {
-    /*class ArduinoBoard : Chetch.Utilities.SerialPortDevice
-    {
-        protected override string getPortName()
-        {
-            String portName;
-            if(OperatingSystem.IsMacOS())
-            {
-                //searchFor = "u-blox 7 - GPS/GNSS Receiver";
-                //searchKey = "USB Product Name";
-                int pid = 0x0043;
-                searchFor = pid.ToString();
-                searchKey = "idProduct";
-                return GetPortNameForDevice(pid, idProduct); 
-            }
-            else if(OperatingSystem.IsLinux())
-            {
-                //searchFor = "usb-u-blox"; //Full name: usb-u-blox_AG_-_www.u-blox.com_u-blox_7_-_GPS_GNSS_Receiver-if00 
-                //searchFor = "usb-Arduino"; //Full name: usb-Arduino__www.arduino.cc__0043_55936343034351B0A061-if00
-                searchFor = "usb-1a86_USB_Serial"; //Full name (generic arduino nano): usb-1a86_USB_Serial-if00-port0
-                return GetPortNameForDevice(searchFor); 
-            }
-            else
-            {
-                throw new Exception(String.Format("Operation system {0} is not supported", Environment.OSVersion.Platform));
-            }
-            return null;
-        }
-    }*/
-
+    
 
     static void Main(string[] args)
     {
